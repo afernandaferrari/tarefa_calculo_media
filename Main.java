f081c9c599b8b473113d51e877900d875bcd22bf
Fernanda Ferrari <fernandaferrari72@gmail.com>
2022-12-24 11:21:41 -0300

    commit inicial

---
+++ Main.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) {
+    
+        System.out.println("---- Cálculo de Médias ----");
+        int med1 = 20;
+        int med2 = 30;
+        int med3 = 55;
+        int med4 = 82;
+        int quant = 4;
+
+        int media = (med1 + med2 + med3 + med4) / quant;
+
+        System.out.println("Média final: " + media);
+    }
